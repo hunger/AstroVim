a35f8bc51a1c2d63b2f267ac6318e0f8f0093fac
Tobias Hunger <tobias.hunger@gmail.com>
2022-03-16 23:42:46 +0100

    Remove todo plugin: TS comment does that already

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -151,21 +151,6 @@ local config = {
         }
       end,
     },
-
-    -- TODO highlighter
-    {
-      "AmeerTaweel/todo.nvim",
-      -- "folke/todo-comments.nvim",
-      requires = "nvim-lua/plenary.nvim",
-      config = function()
-        require("todo").setup {
-          -- require("todo-comments").setup {
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
-        }
-      end,
-    },
   },
 
   overrides = {
