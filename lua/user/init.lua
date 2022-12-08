d7f63aa281c3886114e6e932a771ab0f11afe9b8
Tobias Hunger <tobias.hunger@slint-ui.com>
2022-12-08 17:36:20 +0100

    Use DAP as set up by AstronVim

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -94,49 +94,6 @@ local config = {
       -- Extended file type support
       { "sheerun/vim-polyglot" },
 
-      -- DAP:
-      { "mfussenegger/nvim-dap" },
-      {
-        "rcarriga/nvim-dap-ui",
-        requires = { "nvim-dap", "rust-tools.nvim" },
-        config = function()
-          local dapui = require "dapui"
-          dapui.setup {}
-
-          local dap = require "dap"
-          dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
-          dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
-          dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
-
-          -- DAP mappings:
-          local map = vim.api.nvim_set_keymap
-          map("n", "<f5>", ":lua require('dap').continue()<cr>", { desc = "Continue" })
-          map("n", "<f10>", ":lua require('dap').step_over()<cr>", { desc = "Step over" })
-          map("n", "<f11>", ":lua require('dap').step_into()<cr>", { desc = "Step into" })
-          map("n", "<f12>", ":lua require('dap').step_out()<cr>", { desc = "Step out" })
-          map("n", "<leader>bp", ":lua require('dap').toggle_breakpoint()<cr>", { desc = "Toggle breakpoint" })
-          map(
-            "n",
-            "<leader>Bp",
-            ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>",
-            { desc = "Set conditional breakpoint" }
-          )
-          map(
-            "n",
-            "<leader>lp",
-            ":lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Logpoint message: '))<cr>",
-            { desc = "Set logpoint" }
-          )
-          map("n", "<leader>rp", ":lua require('dap').repl.open()<cr>", { desc = "Open REPL" })
-          map("n", "<leader>RR", ":lua require('dap').run_last()<cr>", { desc = "Run last debugged program" })
-          map("n", "<leader>XX", ":lua require('dap').terminate()<cr>", { desc = "Terminate program being debugged" })
-          map("n", "<leader>du", ":lua require('dap').up()<cr>", { desc = "Up one frame" })
-          map("n", "<leader>dd", ":lua require('dap').down()<cr>", { desc = "Down one frame" })
-        end,
-      },
-      {
-        "mfussenegger/nvim-dap-python",
-      },
       -- Rust support
       {
         "simrat39/rust-tools.nvim",
@@ -178,11 +135,11 @@ local config = {
         config = function() require("typescript").setup { server = astronvim.lsp.server_settings "tsserver" } end,
       },
 
-      {
-        "hrsh7th/cmp-calc",
-        after = "nvim-cmp",
-        config = function() astronvim.add_cmp_source { name = "calc", priority = 100 } end,
-      },
+      -- {
+      --   "hrsh7th/cmp-calc",
+      --   after = "nvim-cmp",
+      --   config = function() astronvim.add_cmp_source { name = "calc", priority = 100 } end,
+      -- },
       {
         "f3fora/cmp-spell",
         after = "nvim-cmp",
@@ -327,7 +284,7 @@ local config = {
     end
 
     _G.hu_toggle_qf = function()
-      if hu_is_qf_open() then
+      if _G.hu_is_qf_open() then
         vim.cmd "cclose"
       else
         vim.cmd "copen"
